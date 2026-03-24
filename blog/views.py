581d585f00e48d2a8bc367bdbbefdeebebc3decc
higoldi31 <goldiikumari71@gmail.com>
2026-03-25 01:49:29 +0530

    create model for posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse(request,'hello this is index page')
+    return render(request,'blog/index.html')
