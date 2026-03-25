44938a2837814c615b814af25c436a633bb3fea6
higoldi31 <goldiikumari71@gmail.com>
2026-03-25 23:34:47 +0530

    debug sign up functionality

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,36 @@
+
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class SignUpViewTests(TestCase):
+    def test_successful_signup_creates_user_and_redirects(self):
+        response = self.client.post(
+            reverse('blog-sign-up'),
+            {
+                'username': 'newuser',
+                'email': 'newuser@example.com',
+                'password1': 'StrongPass123!',
+                'password2': 'StrongPass123!',
+            },
+        )
+
+        self.assertRedirects(response, reverse('blog-index'))
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_invalid_signup_renders_form_errors(self):
+        response = self.client.post(
+            reverse('blog-sign-up'),
+            {
+                'username': 'newuser',
+                'email': 'newuser@example.com',
+                'password1': 'StrongPass123!',
+                'password2': 'Mismatch123!',
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'errorlist')
+        self.assertEqual(User.objects.count(), 0)
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 # Create your views here.
 def sign_up(request):
     if request.method=='POST':
-        form=SignUpForm()
-        if form.is_valid:
+        form=SignUpForm(request.POST)
+        if form.is_valid():
             form.save()
             return redirect('blog-index')
     else:
